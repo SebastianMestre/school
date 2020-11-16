1b9aef62f26ac1ac6cdf77b9bca2be5cc8757f94
santicoronel <santi.loga00@gmail.com>
2020-11-15 21:45:05 -0300

    Comenta

--- io.py
+++ io.py
@@ -1,11 +1,17 @@
+# Helper para input/output y manipulacion de archivos
 
 
+# String -> [String]
 def leer_lemario(path):
     #TODO
     return ['']
+
+# String -> RegistroCompleto
 def leer_registro(path):
     #TODO
     return {}
+
+# String -> None
 def escribir_registro(path, registro):
     #TODO
     return
