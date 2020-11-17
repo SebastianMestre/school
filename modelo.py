e083d0cfa1c8d6a9243b0151a1bc95355c1bdca1
santicoronel <santi.loga00@gmail.com>
2020-11-16 23:03:38 -0300

    Agrega modelo de datos

---
+++ modelo.py
@@ -0,0 +1,22 @@
+# Modelo de datos
+
+# None -> RegistroCompleto
+# Retorna un registro completo vacio
+def new_registro(): 
+    return dict()
+
+# None -> RegistroJugador
+# Retorna un registro de jugador vacio
+def new_jugador(): 
+    return dict()
+
+# [String] -> ResultadoPartida
+# Toma una lista del tipo [palabra, resultado, intentos] y retorna un ResultadoPartida
+def new_partida(val):
+    return (val[1], val[2])
+
+# RegistroJugador String ResultadoPartida -> RegistroJugador
+# Añade un ResultadoPartida al registro de jugador
+def agregar_partida(registro_jugador, palabra, resultado_partida):
+    registro_jugador[palabra] = resultado_partida
+    return registro_jugador
