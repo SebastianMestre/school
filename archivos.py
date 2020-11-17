3aeda1be9162def489b589b74d8077b51428edb9
santicoronel <santi.loga00@gmail.com>
2020-11-17 00:19:12 -0300

    Implementa 'leer_registro'

--- archivos.py
+++ archivos.py
@@ -1,5 +1,6 @@
 # Helper para manipulacion de archivos
 
+import modelo
 
 
 # Archivo -> [String]
@@ -11,8 +12,32 @@ def leer_lemario(path):
 # Archivo -> RegistroCompleto
 # Toma un archivo de registro y retorna un registro completo
 def leer_registro(path):
-    #TODO
-    return {}
+    sep = ','                                   # separador de valores en la entrada
+
+    registro_completo = modelo.new_registro()   # crea registro de juego
+
+    # lee del archivo de registro
+    lines = leer_lineas(path)
+    # itera sobre el contenido del archivo
+    for line in lines:
+        if es_nombre_jugador(line):                             # si la linea corresponde al nombre de un jugador
+            jugador = line                                          # guarda el nombre en 'jugador'
+            registro_completo[jugador] = modelo.new_jugador()       # crea un RegistroJugador en el registro completo
+
+        # si la linea no corresponde al nombre de un jugador entonces corresponde a una partida
+        else:                                                   
+            val = line.split(sep)                       # guarda [palabra, resultado, intentos] en val 
+            palabra = val[0]                            
+            resultado_partida = modelo.new_partida(val) # crea nuevo ResultadoPartida a partir del valor leido
+
+            # guarda el RegistroJugador asociado a 'jugador' en 'registro_jugador'
+            registro_jugador = registro_completo[jugador]               
+            # agrega partida a RegistroJugador nuevo
+            registro_jugador_actualizado = modelo.agregar_partida(registro_jugador, palabra, resultado_partida)
+            # actualiza RegistroJugador en el registro completo
+            registro_completo[jugador] = registro_jugador_actualizado
+
+    return registro_completo
 
 # Archivo -> None
 # Toma un archivo y un registro completo y guarda el segundo en el primero
