76698f42e75823439788ab27fcef18803371c20a
santicoronel <santi.loga00@gmail.com>
2020-11-17 23:28:18 -0300

    Interpreta input de partida

--- modelo.py
+++ modelo.py
@@ -13,10 +13,29 @@ def new_jugador():
 # [String] -> ResultadoPartida
 # Toma una lista del tipo [palabra, condicion, intentos] y retorna un ResultadoPartida
 def new_partida(val):
-    return (val[1], val[2])
+    resultado = interpret_resultado(val[1], val[2])
+    return resultado
 
 # RegistroJugador String ResultadoPartida -> RegistroJugador
 # Añade un ResultadoPartida al registro de jugador
 def agregar_partida(registro_jugador, palabra, resultado_partida):
     registro_jugador[palabra] = resultado_partida
     return registro_jugador
+
+# (String, String) -> ResultadoPartida
+# Toma una tupla ('condicion', 'intentos') y retorna un ResultadoPartida
+def interpret_resultado(condicion, intentos):
+    try:
+        intentos = int(intentos)
+        condicion = interpret_condicion(condicion)      # interpreta condicion
+    except ValueError:
+        print('Error en la entrada para el ResultadoPartida')
+        raise
+    return (condicion, intentos)
+
+# 'SI' | 'NO' -> Boolean
+# Determina la condicion de exito
+def interpret_condicion(condicion):
+    if condicion == 'SI': return True
+    elif condicion == 'NO': return False
+    else: raise ValueError
