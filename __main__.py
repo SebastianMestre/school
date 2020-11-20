d055ac07d9f9144051e8c99115d56484ae02718f
santicoronel <santi.loga00@gmail.com>
2020-11-19 21:14:17 -0300

    Comenta

--- __main__.py
+++ __main__.py
@@ -13,6 +13,7 @@ except IndexError:
 try:
     registro = sys.argv[2]
 except IndexError:
+    # si el argumento no fue pasado se toma una ubicacion por default y se notifica al usuario
     print('No se ha ingresado un archivo \'registro\'. El mismo se guardara en \'registro.txt\'\n')
     registro = 'registro.txt'
 
