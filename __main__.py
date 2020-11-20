60172609b7cacb896f533ae0df9fa0dbb642804f
santicoronel <santi.loga00@gmail.com>
2020-11-19 21:06:37 -0300

    Manipula errores: si no hay archivo registro usa nombre por default

--- __main__.py
+++ __main__.py
@@ -2,6 +2,20 @@ import sys
 
 from src.main import main
 
+# intentamos leer el el argumento que corresponde al lemario
+try:
+    lemario = sys.argv[1]
+except IndexError:
+    # si no fue pasado ningun argumento se termina el programa 
+    sys.exit('Error: deben ingresarse la ubicacion de los archivos \'lemario\' y \'registro\' al correr el programa')
+
+# intentamos leer el el argumento que corresponde al lemario
+try:
+    registro = sys.argv[2]
+except IndexError:
+    print('No se ha ingresado un archivo \'registro\'. El mismo se guardara en \'registro.txt\'\n')
+    registro = 'registro.txt'
+
 #--------------------------------------------------		
 # Inicia el juego
-main(sys.argv[1], sys.argv[2])
+main(lemario, registro)
