fdba6817c84bfb29364de490d243da9453209c4e
santicoronel <santi.loga00@gmail.com>
2021-05-28 22:14:25 -0300

    Cambia precedencias

--- src/main.c
+++ src/main.c
@@ -6,12 +6,12 @@ int main (int argc, char** argv) {
 	TablaOps tabla = tabla_ops_crear();
 	
 	cargar_operador(&tabla, "+", 2, suma, 0);
-	cargar_operador(&tabla, "-", 2, resta, 0);
-	cargar_operador(&tabla, "--", 1, opuesto, 1);
-	cargar_operador(&tabla, "*", 2, producto, 2);
-	cargar_operador(&tabla, "/", 2, division, 3);
-	cargar_operador(&tabla, "%", 2, modulo, 2);
-	cargar_operador(&tabla, "^", 2, potencia, 4);
+	cargar_operador(&tabla, "-", 2, resta, 1);
+	cargar_operador(&tabla, "--", 1, opuesto, 2);
+	cargar_operador(&tabla, "*", 2, producto, 3);
+	cargar_operador(&tabla, "%", 2, modulo, 4);
+	cargar_operador(&tabla, "/", 2, division, 5);
+	cargar_operador(&tabla, "^", 2, potencia, 6);
 
 	interpretar(&tabla);
 
