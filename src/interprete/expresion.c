884f3f72e5140c1ca9a91a1f28d3dc4c94b83749
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2021-05-29 18:46:46 -0300

    Constructores para expresiones (#10)
    * agrega constructores de expresiones
    
    * Agrega una funcion de pila para leer el top, hace todas estaticas
    
    * usa constructores de expresiones en el parser
    
    * hace mas prolija la limpieza de errores en el parser

--- src/interprete/expresion.c
+++ src/interprete/expresion.c
@@ -1,9 +1,40 @@
 #include "expresion.h"
 
+#include <stdlib.h>
+
+static Expresion* expresion_reservar(Expresion datos) {
+	Expresion* resultado = malloc(sizeof(Expresion));
+	*resultado = datos;
+	return resultado;
+}
+
+Expresion* expresion_numero(int valor) {
+	return expresion_reservar((Expresion){
+		.tag = X_NUMERO,
+		.valor = valor,
+	});
+}
+
+Expresion* expresion_alias(char const* alias, int alias_n) {
+	return expresion_reservar((Expresion){
+		.tag = X_ALIAS,
+		.alias = alias,
+		.valor = alias_n,
+	});
+}
+
+Expresion* expresion_operacion(EntradaTablaOps* op, Expresion* sub0, Expresion* sub1) {
+	return expresion_reservar((Expresion){
+		.tag = X_OPERACION,
+		.op = op,
+		.sub = {sub0, sub1},
+	});
+}
+
 void expresion_limpiar(Expresion* expresion) {
-	if (expresion) {
-		expresion_limpiar(expresion->sub[0]);
-		expresion_limpiar(expresion->sub[1]);
-		free(expresion);
-	}
+	if (!expresion)
+		return;
+	expresion_limpiar(expresion->sub[0]);
+	expresion_limpiar(expresion->sub[1]);
+	free(expresion);
 }
