492095d4018cb1d40feb2740b0436b3520fae4e8
santicoronel <santi.loga00@gmail.com>
2021-06-01 22:42:35 -0300

    Documenta errores.

--- src/interprete/error.h
+++ src/interprete/error.h
@@ -5,10 +5,10 @@
 typedef enum {
 	E_PARSER_ALIAS, 			// se esperaba un alias
 	E_PARSER_CARGA, 			// sintaxis en la carga
-	E_PARSER_EXPRESION,	// expresion malformada
-	E_PARSER_OPERACION, 	// error en el grammar  
+	E_PARSER_EXPRESION,	  // expresion malformada
+	E_PARSER_OPERACION, 	// operacion invalida  
 	E_PARSER_VACIA, 			// expresion vacia
-  E_INTERPRETE_EVAL,
+  E_INTERPRETE_EVAL,    // error en la evaluacion del alias
 } ErrorTag;
 
 #endif // ERROR_H
