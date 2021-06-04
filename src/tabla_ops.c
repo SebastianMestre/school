2066388c57a6e43d2f0229fd64dab37463b26920
santicoronel <santi.loga00@gmail.com>
2021-06-04 16:19:57 -0300

    Chequea que la operacion no este definida.

--- src/tabla_ops.c
+++ src/tabla_ops.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
 
 TablaOps tabla_ops_crear() {
 	return (TablaOps){ NULL };
@@ -18,15 +19,27 @@ void tabla_ops_limpiar(TablaOps* tabla) {
 	}
 }
 
+static int existe_simbolo(TablaOps* tabla, char const* simbolo) {
+	for (EntradaTablaOps* it = tabla->entradas; it; it = it->sig)
+		if (strcmp(simbolo, it->simbolo) == 0) return 1;
+	return 0;
+}
+
 void cargar_operador(TablaOps* tabla, char const* simbolo, int aridad, 
 	FuncionEvaluacion eval, int precedencia) {
-	// NICETOHAVE: verificar que no exista el mismo simbolo
 	// NICETOHAVE: chequear nulls
+
+	// Chequeamos que la nueva operacion sea valida.
 	if (aridad != 1 && aridad != 2) {
 		puts("ERROR: las operaciones deben tener aridad 1 o 2.");
 		printf("En operacion \'%s\'; ", simbolo);
 		fflush(stdout); assert(0);
 	}
+	if (existe_simbolo(tabla, simbolo)) {
+		printf("ERROR: la operacion \'%s\' ya esta definida.\n", simbolo);
+		fflush(stdout); assert(0);
+	}
+
 	EntradaTablaOps* nuevaEntrada = malloc(sizeof(*nuevaEntrada));
 	*nuevaEntrada = (EntradaTablaOps){
 		.sig = tabla->entradas,
