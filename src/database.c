1972a37d081658f02dea88062be92c42ed5f60fd
SebastianMestre <mestresebastianunr@gmail.com>
2021-06-25 13:00:20 -0300

    implement database_create

--- src/database.c
+++ src/database.c
@@ -1,3 +1,11 @@
 #include "database.h"
 
 #include "storage.h"
+
+Database
+database_create(Storage* storage) {
+	return (Database) {
+		.index = index_create(storage),
+		.history = history_create(storage),
+	};
+}
