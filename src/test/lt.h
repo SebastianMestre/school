4cc8994168be23fad454049f406e5cb13705bd69
SebastianMestre <mestresebastianunr@gmail.com>
2021-06-26 20:21:54 -0300

    add a prng to libtest

--- src/test/lt.h
+++ src/test/lt.h
@@ -30,10 +30,20 @@
 static bool lt_ran[LT_MAX_TEST_COUNT];
 static int lt_test_count = 0;
 static int lt_assertions_ran = 0;
+static int lt_rand_seed = 0;
 
 static void lt_reset() {
 	memset(lt_ran, 0, sizeof(lt_ran));
 	lt_assertions_ran = 0;
+	lt_rand_seed = 0;
+}
+
+static void lt_srand(int new_seed) {
+	lt_rand_seed = new_seed;
+}
+static int lt_rand() {
+	// PRNG robado de internet (donde???)
+	return lt_rand_seed = (lt_rand_seed * 1103515245 + 12345) & ((1U << 31) - 1);
 }
 
 static void lt_report(char const* module_name) {
@@ -43,4 +53,15 @@ static void lt_report(char const* module_name) {
 	printf(
 		"%s tests passed -- %d tests ran with %d assertions \n",
 		module_name, tests_ran, lt_assertions_ran);
+}
+
+static void lt_do_not_call_a();
+static void lt_do_not_call_b() {
+	lt_do_not_call_a();
+}
+
+static void lt_do_not_call_a() {
+	lt_do_not_call_b();
+	lt_rand();
+	lt_srand(0);
 }
