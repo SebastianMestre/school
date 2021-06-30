63fd099d3da318b5f89e45edc97dd8054e3db984
SebastianMestre <mestresebastianunr@gmail.com>
2021-06-30 01:21:04 -0300

    take quicksort back to the stone age

--- src/quicksort.c
+++ src/quicksort.c
@@ -6,43 +6,40 @@
 #include <assert.h>
 
 static void
-iter_swap(Iter lhs, Iter rhs) {
-	assert(lhs.element_width == rhs.element_width);
-	if (lhs.pos == rhs.pos)
+mem_swap(void* lhs, void* rhs, size_t n) {
+	if (lhs == rhs)
 		return;
-	uint8_t* lhs_data = lhs.pos;
-	uint8_t* rhs_data = rhs.pos;
-	size_t element_width = lhs.element_width;
-	for (size_t i = 0; i != element_width; ++i) {
-		lhs_data[i] ^= rhs_data[i];
-		rhs_data[i] ^= lhs_data[i];
-		lhs_data[i] ^= rhs_data[i];
+	for (size_t i = 0; i != n; ++i) {
+		((uint8_t*)lhs)[i] ^= ((uint8_t*)rhs)[i];
+		((uint8_t*)rhs)[i] ^= ((uint8_t*)lhs)[i];
+		((uint8_t*)lhs)[i] ^= ((uint8_t*)rhs)[i];
 	}
 }
 
-static Iter
-partition(Iter i, Iter j, Iter pivot, Comparator cmp) {
-	Iter k = i;
-	for (; !iter_eq(i, j); iter_advance(&i)) {
-		if (call_cmp(cmp, i.pos, pivot.pos) < 0) {
-			iter_swap(i, k);
-			iter_advance(&k);
+static void*
+partition(void* i, void* j, void* pivot, size_t size, Comparator cmp) {
+	void* k = i;
+	for (; i != j; i += size) {
+		if (call_cmp(cmp, i, pivot) < 0) {
+			mem_swap(i, k, size);
+			k += size;
 		}
 	}
 	return k;
 }
 
 static void
-quicksort_impl(Iter l, Iter r, Comparator cmp) {
-	if (iter_distance(l, r) <= 1)
+quicksort_impl(void* l, void* r, size_t size, Comparator cmp) {
+	assert(l <= r);
+	if ((size_t)(r - l) <= size)
 		return;
-	Iter partition_point = partition(iter_next(l), r, l, cmp);
-	iter_swap(l, iter_prev(partition_point));
-	quicksort_impl(l, iter_prev(partition_point), cmp);
-	quicksort_impl(partition_point, r, cmp);
+	void* cut = partition(l + size, r, l, size, cmp);
+	mem_swap(l, cut - size, size);
+	quicksort_impl(l, cut - size, size, cmp);
+	quicksort_impl(cut, r, size, cmp);
 }
 
 void
 quicksort(Array arr, Comparator cmp) {
-	quicksort_impl(array_begin(&arr), array_end(&arr), cmp);
+	quicksort_impl(array_begin(&arr).pos, array_end(&arr).pos, arr.element_width, cmp);
 }
