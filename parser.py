1ae01b1150a5c10c6bf683e6eed1d3c6402455cd
SebastianMestre <mestresebastianunr@gmail.com>
2022-01-23 16:36:28 -0300

    Completa el parser

--- parser.py
+++ parser.py
@@ -1,97 +1,90 @@
-
+import sys
 import re
 
-def read_graph(filePath):
-  """ Lee un grafo desde un archivo. """
-  # Creamos un grafo vacío
-  graph = ([], [])
-  # Intentamos abrir el archivo
-  try:
-    with open(filePath) as fp:
-      # Convertimos el archivo a una lista
-      lines = []
-      for line in fp:
-        # Ignoramos espacios al principio y al final del renglón
-        line = line.strip()
-        # Ignoramos renglones vacíos
-        if line != '':
-          lines.append(line)
-    # Chequeamos que el archivo no es vacío
-    size = len(lines)
-    if size == 0:
-      print("Error: file is empty.")
-      sys.exit()
-    # Determinamos la cantidad de nodos en lo posible
+def read_graph(path):
     try:
-      n = int(lines[0])
-      # Chequeamos que hay suficientes nodos
-      if n >= size:
-        print("Error: not enough nodes.")
-        sys.exit()
-      # Chequamos que el número de nodos es válido
-      if n < 0:
-        print("Error: invalid number of nodes.")
-        sys.exit()
-      # Inicializamos las variables para leer los renglones
-      invalid = False
-      repeated = False
-      i = 1
-      # Leemos los vértices
-      nodes = set()
-      while i <= n and not invalid and not repeated:
-        node = lines[i]
-        # Chequeamos que el nombre del nodo no tenga espacios
-        if re.search(r"\s", node):
-          invalid = True
-        # Chequeamos que no haya nodos repetidos
-        elif node in nodes:
-          repeated = True
-        # Si es válido, agregamos el nodo
-        else:
-          graph[0].append(node)
-          nodes.add(node)
-        i += 1
-      # Chequamos que los nodos son válidos
-      if invalid:
-        print("Error: invalid node.")
-        sys.exit()
-      # Chequeamos que todos los nodos son únicos
-      if repeated:
-        print("Error: repeated nodes.")
-        sys.exit()
-      # Leemos las aristas
-      edges = set()
-      while i < size and not invalid and not repeated:
-        edge = tuple(lines[i].split())
-        # Chequeamos que haya dos nodos válidos distintos
-        if len(edge) != 2 or edge[0] == edge[1] or not edge[0] in nodes or not edge[1] in nodes:
-          invalid = True
-        # Chequeamos que no haya aristas repetidas
-        elif edge in edges:
-          print("ARISTA REPE")
-          print(edge)
-          repeated = True
-        # Si es válido, agregamos la arista
-        else:
-          graph[1].append(edge)
-          edges.add(edge)
-          edges.add(edge[::-1])
-        i += 1
-      # Chequeamos que las aristas son válidas
-      if invalid:
-        print("Error: invalid edge.")
-        sys.exit()
-      # Chequamos que las aristas son únicas
-      if repeated:
-        print("Error: repeated edge.")
-        sys.exit()
-    # Si no se proporciona el número de nodos al inicio, terminamos la ejecución
+        file_content = read_file(path)
+    except OSError:
+        show_error("No se pudo leer el archivo.")
+    return parse_graph(file_content)
+
+def parse_graph(file_content):
+    lines = split_lines(file_content)
+
+    if len(lines) == 0:
+        show_error("El archivo no tiene contenido.")
+
+    try:
+        n = int(lines[0])
     except ValueError:
-      print("Error: number of nodes required.")
-      sys.exit()
-  # Si no se pudo abrir el archivo, terminamos la ejecución
-  except OSError:
-    print("Error: file cannot be opened.")
+        show_error("La primera linea del archivo no contiene exactamente un entero.")
+
+    if n < 0:
+        show_error("La cantidad de nodos debe ser no negativa.")
+
+    content_lines = lines[1:]
+
+    if len(content_lines) < n:
+        show_error("Se esperaban {} nodos, pero hay a lo sumo {}.".format(n, len(content_lines)))
+
+    nodes = parse_nodes(content_lines[:n])
+    edges = parse_edges(content_lines[n:], nodes)
+    return (list(nodes), list(edges))
+
+def parse_nodes(lines):
+    nodes = set()
+    for node in lines:
+        error = validate_node(node, nodes)
+        if error is not None:
+            show_error(error)
+        nodes.add(node)
+    return nodes
+
+def validate_node(node, nodes):
+    if re.search(r"\s", node):
+        return "Se encontro un nodo con un espacio: {}".format(node)
+
+    if node in nodes:
+        return "Se encontro un nodo repetido: {}".format(node)
+
+    return None
+
+def parse_edges(lines, nodes):
+    edges = set()
+    for line in lines:
+        components = line.split()
+        error = validate_edge(components, nodes, edges, line)
+        if error is not None:
+            show_error(error)
+        edges.add(tuple(components))
+    return edges
+
+def validate_edge(components, nodes, edges, line):
+    if len(components) != 2:
+        return "Se encontro una arista con {} componente/s: {}".format(len(components), line)
+
+    if components[0] not in nodes or components[1] not in nodes:
+        return "Se encontro una arista que referencia nodos que no existen: {}".format(line)
+
+    if components[0] == components[1]:
+        return "Se encontro un lazo: {}".format(line)
+
+    edge = tuple(components)
+
+    if edge in edges or edge[::-1] in edges:
+        return "Se encontro una arista repetida: {}".format(line)
+
+    return None
+
+def show_error(text):
+    print("[ Error ] {}".format(text))
     sys.exit()
-  # Retornamos el grafo
-  return graph
+
+def split_lines(file_content):
+    lines = file_content.splitlines()
+    trimmed_lines = [line.strip() for line in lines]
+    return [line for line in trimmed_lines if line != '']
+
+def read_file(path):
+    with open(path, "r") as fp:
+        return fp.read()
