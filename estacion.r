8c646f33ea9b3c8a4dfe4519c068220098821f6f
santicoronel <santi.loga00@gmail.com>
2022-05-06 13:55:25 -0300

    Hace grafico de estaciones

--- estacion.r
+++ estacion.r
@@ -19,8 +19,21 @@ pie(c(dim(recorridos[mismaEstacion,])[1] / dim(recorridos)[1],
         col = c("blue", "red"),
         labels = c("Misma estacion", "Diferente estacion"))
 
-##
-### Faltaria hacer grafico de bastones (cant de viajes, cant de estaciones)
-### no tengo ni idea cual seria la forma mas facil
+
 ### plot(vector) hace un grafico de bastones ;)
-##
+
+freq_estacion_origen <- table(recorridos$direccion_estacion_origen) 
+freq_viajes_origen <- table(freq_estacion_origen)
+plot(freq_viajes_origen, 
+     xlab = "Cantidad de viajes", 
+     ylab = "Cantidad de estaciones",
+     main = "Cantidad de estaciones origen de x viajes")
+
+freq_estacion_destino <- table(recorridos$direccion_estacion_destino) 
+freq_viajes_destino <- table(freq_estacion_destino)
+### Tal vez estaria bueno limitar x para q no se vean los outliers
+plot(freq_viajes_destino, 
+     xlab = "Cantidad de viajes", 
+     ylab = "Cantidad de estaciones",
+     main = "Cantidad de estaciones destino de x viajes")
+
