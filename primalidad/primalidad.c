44dbc3170889400f113c2081d3bc699710b8a5f8
SebastianMestre <mestresebastianunr@gmail.com>
2022-05-25 12:07:12 -0300

    cambia fors para que incluyan sqrt(n), invierte resultado de las funciones

--- primalidad/primalidad.c
+++ primalidad/primalidad.c
@@ -4,25 +4,25 @@
 #include <math.h>
 #include "timing.h"
 
-int primo_paralelo(int n) {
-	int es_primo = 0;
-	#pragma omp parallel for reduction(|:es_primo)
-	for (int i = 2; i < (int) sqrt(n); i++) {
-		es_primo |= (n % i == 0);
+int primo_paralelo(long long n) {
+	int es_compuesto = 0;
+	#pragma omp parallel for reduction(|:es_compuesto)
+	for (int i = 2; i <= (int) sqrt(n); i++) {
+		es_compuesto |= (n % i == 0);
 	}
-	return es_primo;
+	return !es_compuesto;
 }
 
-int primo_secuencial(int n) {
-	int es_primo = 0;
-	for (int i = 2; i < (int) sqrt(n); i++) {
-		es_primo |= (n % i == 0);
+int primo_secuencial(long long n) {
+	int es_compuesto = 0;
+	for (int i = 2; i <= (int) sqrt(n); i++) {
+		es_compuesto |= (n % i == 0);
 	}
-	return es_primo;
+	return !es_compuesto;
 }
 
 int main() {
-	int n = random();
+	long long n = (long long)random() * RAND_MAX + (long long)random();
 
 	int paralelo = TIME(primo_paralelo(n), NULL);
 	int secuencial = TIME(primo_secuencial(n), NULL);
