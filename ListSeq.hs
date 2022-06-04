f9604b88a23a3a601625e5f3928815c3a3ba7b07
SebastianMestre <mestresebastianunr@gmail.com>
2022-06-04 19:04:47 -0300

    super indentacion loca

--- ListSeq.hs
+++ ListSeq.hs
@@ -4,51 +4,39 @@ import Seq
 import Par
 
 instance Seq [] where
-  emptyS       = []
-
-  singletonS a = [a]
-
-  lengthS = length
-
-  nthS = (!!)
-
+  emptyS        = []
+  singletonS a  = [a]
+  lengthS       = length
+  nthS          = (!!)
   tabulateS f n = mapS f [0..(n-1)]
-
-  mapS f s = go s
+  mapS f s      = go s
     where
       go [] = []
       go (x:xs) = let
         (y, ys) = f x ||| go xs
         in y:ys
-
-  filterS p s = go s
+  filterS p s   = go s
     where
       go [] = []
       go (x:xs) = let
         (b, ys) = p x ||| go ys
           in if b then x:ys else ys
-
-  appendS = (++)
-
-  takeS [] _                 = []
-  takeS (x:xs) n | n <= 0    = []
-                 | otherwise = head s : take (tail s) (n-1)
-
-  dropS = flip drop
-
-  showtS []  = EMPTY
-  showtS [x] = ELT x
-  showtS xs  = NODE lhs rhs
-    where (lhs, rhs) = splitAt (n `div` 2) xs
-          n = length xs
-
-  showlS [] = NIL
+  appendS       = (++)
+  takeS [] _    = []
+  takeS (x:xs) n
+    | n <= 0    = []
+    | otherwise = x : takeS xs (n-1)
+  -- takeS         = flip take -- Vale esto ???
+  dropS         = flip drop
+  showtS []     = EMPTY
+  showtS [x]    = ELT x
+  showtS xs     = NODE lhs rhs
+    where
+      (lhs, rhs) = splitAt (n `div` 2) xs
+      n          = length xs
+  showlS []     = NIL
   showlS (x:xs) = CONS x xs
-
-  joinS      = undefined
-
-  reduceS    = undefined
-
-  scanS      = undefined
-
-  fromList = id
+  joinS         = undefined
+  reduceS       = undefined
+  scanS         = undefined
+  fromList      = id
