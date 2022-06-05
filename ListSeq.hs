2b46e3a633ae6502875ef3c2c21467df52affd56
SebastianMestre <mestresebastianunr@gmail.com>
2022-06-05 20:28:54 -0300

    implementa scan y reduce para ListSeq

--- ListSeq.hs
+++ ListSeq.hs
@@ -22,12 +22,12 @@ instance Seq [] where
         (b, ys) = p x ||| go ys
           in if b then x:ys else ys
   appendS       = (++)
-  takeS [] _    = []
-  takeS (x:xs) n
-    | n <= 0    = []
-    | otherwise = x : takeS xs (n-1)
-  -- takeS         = flip take -- Vale esto ???
-  dropS         = flip drop
+  -- takeS [] _    = []
+  -- takeS (x:xs) n
+  --   | n <= 0    = []
+  --   | otherwise = x : takeS xs (n-1)
+  takeS         = flip take -- Vale esto ???
+  dropS         = flip drop -- Vale esto ???
   showtS []     = EMPTY
   showtS [x]    = ELT x
   showtS xs     = NODE lhs rhs
@@ -37,6 +37,29 @@ instance Seq [] where
   showlS []     = NIL
   showlS (x:xs) = CONS x xs
   joinS         = undefined
-  reduceS       = undefined
-  scanS         = undefined
+
+  reduceS f b [] = b
+  reduceS f b s  = f b $ go s
+    where
+      go [x] = x
+      go xs  = go $ contractS f s
+
+  scanS f b s   = go s
+    where
+      go []  = ([], b)
+      go [x] = ([b], f b x)
+      go s   = (expandList s s', p)
+        where
+          (s', p)            = go $ contractS f s
+
   fromList      = id
+
+contractList f []        = []
+contractList f [x]       = [x]
+contractList f (x:x':xs) = y:ys
+  where (y, ys) = f x x' ||| contractList f xs
+
+expandList f [] _            = []
+expandList f [_] (y:_)       = [y]
+expandList f (x:_:xs) (y:ys) = y:z:zs
+  where (z, zs) = f y x ||| expandList f xs ys
