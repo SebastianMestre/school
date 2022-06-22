c9d3a3746a25b710abebe3b948354e8d09532dd1
Sebastián Mestre <36825825+SebastianMestre@users.noreply.github.com>
2022-06-22 11:24:58 -0300

    finaliza interfaz kv_store, crea implementacion lenta

--- server/kv_store.h
+++ server/kv_store.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#define KV_STORE_OK 0
+#define KV_STORE_OOM (-1)
+#define KV_STORE_NOTFOUND (-2)
+#define KV_STORE_UNK (-3)
+
+#include <stddef.h>
+
 struct kv_store;
+
+struct kv_store* kv_store_init();
 
 // pide que se libere espacio de memoria
 //
@@ -12,7 +21,7 @@ struct kv_store;
 int kv_store_evict(struct kv_store* store);
 
 // pide que se inserte un par (clave, valor)
-// puede liberar otras entradas del almacenamiento, incluso en el caso de error
+// si tiene exito, toma ownership de la clave y el valor
 //
 // PARAMS
 // key: la clave a insertar
@@ -24,27 +33,56 @@ int kv_store_evict(struct kv_store* store);
 // return: devuelve un numero negativo si se produce un error y 0 en caso contrario
 // 
 // ERRORES
-// no hay suficiente espacio, incluso liberando otras entradas
-int kv_store_put(struct kv_store* store, char const* key, int key_length, char const* value, int value_length);
+// no hay suficiente espacio de heap para insertar un nodo nuevo
+int kv_store_put(struct kv_store* store, char* key, size_t key_length, char* value, size_t value_length);
 
-// pide que se inserte un par (clave, valor)
-// puede liberar otras entradas del almacenamiento, incluso en el caso de error
+// pide el valor correspondiente a una clave
+// da ownership sobre el valor que se devuelve
+// no toma ownership de la clave
 //
 // PARAMS
 // key: la clave a buscar
 // key_length: longitud en bytes de la clave
 //
 // RESULT
-// out_value: guarda la direccion del valor que corresponde a la clave. Se debe liberar usando free().
+// out_value: el valor asociado a la clave, 
+// out_value_length: longitud en bytes del valor
+// return: devuelve un numero negativo si se produce un error y 0 en caso contrario
+// 
+// ERRORES
+// no existe la clave en la estructura
+// no hay suficiente memoria para copiar el valor
+int kv_store_get(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length);
+
+// pide que se se elimine la entrada correspondiente a una clave
+// no toma ownership de la key
+//
+// PARAMS
+// key: la clave a buscar
+// key_length: longitud en bytes de la clave
+//
+// RESULT
 // return: devuelve un numero negativo si se produce un error y 0 en caso contrario.
 // 
 // ERRORES
-// no hay suficiente espacio, incluso liberando otras entradas
-int kv_store_get(struct kv_store* store, char const* key, int key_length, char** out_value, int* out_value_length);
+// no existe la clave en la estructura
+int kv_store_del(struct kv_store* store, char* key, size_t key_length);
 
-// toma ownership del key y el value
-int kv_store_put(struct kv_store* store, char const* key, int key_length);
-
-int kv_store_take(struct kv_store* store, char const* key, int key_length, char** out_value, int* out_value_length);
+// pide el valor correspondiente a una clave, y que se elimine la entrada correspondiente
+// no toma ownership de la key
+// da ownership sobre la clave y el valor que se devuelven
+//
+// PARAMS
+// key: la clave a buscar
+// key_length: longitud en bytes de la clave
+//
+// RESULT
+// out_value: el valor asociado a la clave, 
+// out_value_length: longitud en bytes del valor
+// return: devuelve un numero negativo si se produce un error y 0 en caso contrario
+// 
+// ERRORES
+// no existe la clave en la estructura
+int kv_store_take(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length);
 
 int kv_store_stat(struct kv_store* store, int* out_stat);
---
+++ server/kv_store_array_linear_lookup.c
@@ -0,0 +1,216 @@
+#include "kv_store.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdio.h>
+
+#define KV_STORE_SIZE 1000000
+
+struct kv_entry {
+	char* key;
+	size_t key_length;
+
+	char* value;
+	size_t value_length;
+
+	uint64_t generation;
+};
+
+struct kv_store {
+	pthread_mutex_t m;
+	size_t size;
+	size_t capacity;
+	uint64_t generation;
+	struct kv_entry** entries;
+};
+
+struct kv_store* kv_store_init() {
+	struct kv_store* store = malloc(sizeof(*store));
+	if (!store) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	store->entries = malloc(sizeof(store->entries[0]) * KV_STORE_SIZE);
+	if (!store->entries) {
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+	memset(store->entries, 0, sizeof(store->entries[0]) * KV_STORE_SIZE);
+
+	store->generation = 0;
+	store->size = 0;
+	store->capacity = KV_STORE_SIZE;
+
+	pthread_mutex_init(&store->m, NULL);
+
+	return store;
+}
+
+static struct kv_entry* kv_entry_init(char* key, size_t key_length, char* value, size_t value_length, size_t generation) {
+	struct kv_entry* entry = malloc(sizeof(*entry));
+	if (!entry) {
+		return NULL;
+	}
+
+	entry->key = key;
+	entry->key_length = key_length;
+
+	entry->value = value;
+	entry->value_length = value_length;
+
+	entry->generation = generation;
+
+	return entry;
+}
+
+static void kv_entry_free(struct kv_entry* entry) {
+	free(entry->value);
+	free(entry->key);
+	free(entry);
+}
+
+static struct kv_entry** lookup_key(struct kv_store* store, char* key, size_t key_length) {
+	for (int i = 0; i < store->size; ++i) {
+		struct kv_entry* entry = store->entries[i];
+		if (entry->key_length == key_length && memcmp(entry->key, key, key_length) == 0) {
+			return &store->entries[i];
+		}
+	}
+	return NULL;
+}
+
+static int kv_store_put_impl(struct kv_store* store, char* key, size_t key_length, char* value, size_t value_length) {
+
+	struct kv_entry** old_entry_ptr = lookup_key(store, key, key_length);
+
+	if (old_entry_ptr) {
+		struct kv_entry* old_entry = *old_entry_ptr;
+
+		free(old_entry->key);
+		old_entry->key = key;
+		old_entry->key_length = key_length;
+
+		free(old_entry->value);
+		old_entry->value = value;
+		old_entry->value_length = value_length;
+
+		return KV_STORE_OK;
+	} 
+
+	assert(store->size < store->capacity);
+	struct kv_entry* new_entry = kv_entry_init(key, key_length, value, value_length, store->generation++);
+
+	if (!new_entry) {
+		return KV_STORE_OOM;
+	}
+
+	store->entries[store->size++] = new_entry;
+
+	return KV_STORE_OK;
+}
+
+int kv_store_put(struct kv_store* store, char* key, size_t key_length, char* value, size_t value_length) {
+	pthread_mutex_lock(&store->m);
+
+	int result = kv_store_put_impl(store, key, key_length, value, value_length);
+
+	pthread_mutex_unlock(&store->m);
+
+	return result;
+}
+
+static int kv_store_get_impl(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length) {
+	struct kv_entry** entry_ptr = lookup_key(store, key, key_length);
+
+	if (!entry_ptr) {
+		*out_value = NULL;
+		*out_value_length = 0;
+		return KV_STORE_NOTFOUND;
+	}
+
+	struct kv_entry* entry = *entry_ptr;
+	char* new_value = malloc(entry->value_length);
+
+	if (!new_value) {
+		*out_value = NULL;
+		*out_value_length = 0;
+		return KV_STORE_OOM;
+	}
+
+	memcpy(new_value, entry->value, entry->value_length);
+
+	*out_value = new_value;
+	*out_value_length = entry->value_length;
+
+	return KV_STORE_OK;
+}
+
+int kv_store_get(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length) {
+	pthread_mutex_lock(&store->m);
+
+	int result = kv_store_get_impl(store, key, key_length, out_value, out_value_length);
+
+	pthread_mutex_unlock(&store->m);
+
+	return result;
+}
+
+static int kv_store_del_impl(struct kv_store* store, char* key, size_t key_length) {
+	struct kv_entry** entry_ptr = lookup_key(store, key, key_length);
+
+	if (!entry_ptr) {
+		return KV_STORE_NOTFOUND;
+	}
+
+	struct kv_entry* entry = *entry_ptr;
+	*entry_ptr = store->entries[--store->size];
+	kv_entry_free(entry);
+
+	return KV_STORE_OK;
+}
+
+int kv_store_del(struct kv_store* store, char* key, size_t key_length) {
+	pthread_mutex_lock(&store->m);
+
+	int result = kv_store_del_impl(store, key, key_length);
+
+	pthread_mutex_unlock(&store->m);
+
+	return result;
+}
+
+static int kv_store_take_impl(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length) {
+	struct kv_entry** entry_ptr = lookup_key(store, key, key_length);
+
+	if (!entry_ptr) {
+		return KV_STORE_NOTFOUND;
+	}
+
+	struct kv_entry* entry = *entry_ptr;
+
+	*out_value = entry->value;
+	*out_value_length = entry->value_length;
+
+	entry->value = NULL;
+	entry->value_length = 0;
+
+	*entry_ptr = store->entries[--store->size];
+	kv_entry_free(entry);
+
+	return KV_STORE_OK;
+}
+
+int kv_store_take(struct kv_store* store, char* key, size_t key_length, char** out_value, size_t* out_value_length) {
+	pthread_mutex_lock(&store->m);
+
+	int result = kv_store_take_impl(store, key, key_length, out_value, out_value_length);
+
+	pthread_mutex_unlock(&store->m);
+
+	return result;
+}
