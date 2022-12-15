1d8ef76e73f03725a2739660fc2d861c20a8499b
Santiago Coronel <santi.loga00@gmail.com>
2022-12-15 15:54:19 -0300

    ejercicio 8

--- 8.r
+++ 8.r
@@ -1,11 +1,11 @@
 poisson <- function(l, dt) {
   p <- l*dt*exp(-l*dt)
   x <- runif(1)
-  return (x  < p)
+  return (x < p)
 }
 
 lambda <- 0.1
-kms <- 100
+kms <- 10000
 dt <- 0.1
 n <- kms / dt
 
@@ -16,13 +16,12 @@ D_t <- 0
 for (t in 1:n) {
   if (poisson(lambda, dt)) {
     N_t <- N_t + 1
-    N <- rbind(N, c(t, N_t))
+    N <- rbind(N, c(t*dt, N_t))
     D <- rbind(D, c(N_t, D_t))
     D_t <- 0
   }
-  else {
-    D_t <- D_t + 1
-  }
+  D_t <- D_t + dt
 }
 plot(N, type = "s")
-# plot(D)
+hist(N[,1])
+hist(D, breaks=100)
